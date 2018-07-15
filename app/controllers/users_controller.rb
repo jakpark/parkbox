35f593b16a36a2d3974c719e8d2e28a059bf914c
jake park <jake@parkjake.com>
2018-07-15 13:34:08 -0400

    update user#routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  # skip_before_action :verify_authenticity_token # for API requests
+  skip_before_action :verify_authenticity_token # for API requests
   
   def index
     render plain: 'View: users#index'
   end
   
+  def show
+    render json: params
+  end
+  
   def create
-    render plain: 'view: users#create'
+    render json: params
   end
 end
