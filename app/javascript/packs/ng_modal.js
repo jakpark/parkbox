e6742fc7dee2da199e7e5493296c4953236e4678
jake park <jake@parkjake.com>
2018-07-17 00:02:42 -0400

    rename ng component

--- app/javascript/packs/ng_modal.js
+++ app/javascript/packs/ng_modal.js
@@ -1,9 +1,9 @@
 // Run this Angular example by adding the following HTML markup to your view:
 //
-// <hello-angular>Loading...</hello-angular>
+// <ng_modal>Loading...</ng_modal>
 //
-// <%= javascript_pack_tag 'hello_angular' %>
+// <%= javascript_pack_tag 'ng_modal' %>
 
-require('../hello_angular')
+require('../ng_modal')
 
 console.log('Hello World from Angular')
