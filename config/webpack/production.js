b1b35b6c5b25fd89ca917698ac0062689fec837b
jake park <jake@parkjake.com>
2018-07-17 11:03:45 -0400

    remove production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -8,7 +8,6 @@ const CompressionPlugin = require('compression-webpack-plugin')
 const sharedConfig = require('./shared.js')
 
 module.exports = merge(sharedConfig, {
-  mode: 'production',
   output: { filename: '[name]-[chunkhash].js' },
   devtool: 'source-map',
   stats: 'normal',
